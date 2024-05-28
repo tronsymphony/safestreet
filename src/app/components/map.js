b74aed6f5d9932591eeede2126071c0b46f01b43
nitya <tronsymphony@gmail.com>
2024-05-28 16:12:31 -0700

    map

--- src/app/components/map.js
+++ src/app/components/map.js
@@ -1,8 +1,8 @@
 'use client';
 import React, { useState, useEffect, useCallback } from "react";
-import { GoogleMap, LoadScript, Marker, DirectionsService, DirectionsRenderer, Polyline } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, DirectionsService, DirectionsRenderer, Polyline, BicyclingLayer } from '@react-google-maps/api';
 import Modal from 'react-modal';
-
+import { customMapStyles } from "./customMapStyles"
 
 export default function GMap() {
     const API_KEY = 'AIzaSyBSYN4U7NwpFWZfXmHCMF7jta6SHdMewVY';
@@ -11,22 +11,26 @@ export default function GMap() {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [modalContent, setModalContent] = useState({});
     const [mapLoaded, setMapLoaded] = useState(false);
+    const [clickedLatLng, setClickedLatLng] = useState(null);
 
     const routes = [
         {
             origin: { lat: 33.96153532016564, lng: -118.4580683665798 },
             destination: { lat: 34.02697494372404, lng: -118.37695068714318 },
-            waypoints: [{ location: { lat: 33.96153532016564, lng: -118.4580683665798 } }]
+            waypoints: [{ location: { lat: 33.96153532016564, lng: -118.4580683665798 } }],
+            color: '#FF5733'
         },
         {
             origin: { lat: 33.97539397145512, lng: -118.4325378031858 },
             destination: { lat: 33.96431555913657, lng: -118.4230538223665 },
-            waypoints: [{ location: { lat: 33.97539397145512, lng: -118.4325378031858 } }]
+            waypoints: [{ location: { lat: 33.97539397145512, lng: -118.4325378031858 } }],
+            color: 'darkgreen'
         },
         {
             origin: { lat: 33.979215019959895, lng: -118.46648985815806 },
             destination: { lat: 33.99046011410423, lng: -118.4477936067906 },
-            waypoints: [{ location: { lat: 33.979215019959895, lng: -118.46648985815806 } }]
+            waypoints: [{ location: { lat: 33.979215019959895, lng: -118.46648985815806 } }],
+            color: 'darkgreen'
         }
     ];
 
@@ -48,11 +52,15 @@ export default function GMap() {
         scaleControl: true,
         streetViewControl: true,
         rotateControl: true,
-        fullscreenControl: true
+        fullscreenControl: true,
+        // mapTypeId: 'terrain' 
+        styles: customMapStyles,
     };
 
-    const onLoad = useCallback(() => {
+    const onLoad = useCallback((map) => {
         setMapLoaded(true);
+        // const bikeLayer = new window.google.maps.BicyclingLayer();
+        // bikeLayer.setMap(map);
     }, []);
 
     const handlePolylineClick = (lat, lng) => {
@@ -65,6 +73,13 @@ export default function GMap() {
     };
 
 
+    const handleMapClick = (event) => {
+        const lat = event.latLng.lat();
+        const lng = event.latLng.lng();
+        setClickedLatLng({ lat, lng });
+        setModalContent({ lat, lng });
+        setModalIsOpen(true);
+    };
 
     return (
         <>
@@ -75,6 +90,7 @@ export default function GMap() {
                     mapContainerStyle={{ height: '80vh', width: '100%' }}
                     options={mapOptions}
                     onLoad={onLoad}
+                    onClick={handleMapClick}
                 >
                     {routes.map((route, index) => (
                         <React.Fragment key={index}>
@@ -94,16 +110,28 @@ export default function GMap() {
                         <React.Fragment key={index}>
                             {directions.routes[0].legs.map((leg, legIndex) =>
                                 leg.steps.map((step, stepIndex) => (
-                                    <Polyline
-                                        key={stepIndex}
-                                        path={step.path}
-                                        options={{
-                                            strokeColor: index % 2 === 0 ? '#FF0000' : '#0000FF',
-                                            strokeOpacity: 0.8,
-                                            strokeWeight: 4,
-                                        }}
-                                        onClick={(e) => handlePolylineClick(e.latLng.lat(), e.latLng.lng())}
-                                    />
+                                    <React.Fragment key={stepIndex}>
+                                        {/* Shadow Polyline */}
+                                        <Polyline
+                                            path={step.path}
+                                            options={{
+                                                strokeColor: routes[index].color,// Shadow color
+                                                strokeOpacity: 0.2, // Shadow opacity
+                                                strokeWeight: 5, // Shadow weight (larger than main line)
+                                            }}
+                                            onClick={(e) => handlePolylineClick(e.latLng.lat(), e.latLng.lng())}
+                                        />
+                                        {/* Main Polyline */}
+                                        <Polyline
+                                            path={step.path}
+                                            options={{
+                                                strokeColor: routes[index].color,
+                                                strokeOpacity: .7,
+                                                strokeWeight: 1,
+                                            }}
+                                            onClick={(e) => handlePolylineClick(e.latLng.lat(), e.latLng.lng())}
+                                        />
+                                    </React.Fragment>
                                 ))
                             )}
                         </React.Fragment>
@@ -120,6 +148,7 @@ export default function GMap() {
                 <h2 style={{ color: 'black' }}>Segment Info</h2>
                 <p style={{ color: 'black' }}>Latitude: {modalContent.lat}</p>
                 <p style={{ color: 'black' }}>Longitude: {modalContent.lng}</p>
+                <p style={{ color: 'black' }}>{modalContent.lat}, {modalContent.lng}</p>
                 <button onClick={closeModal}>Close</button>
             </Modal>
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import GMap from './components/map'
-// const API_KEY = 'AIzaSyBSYN4U7NwpFWZfXmHCMF7jta6SHdMewVY';
 export default function Home() {
   return (
     <main className={styles.main}>
